54733e782d452bad14ce950c51459cffe6286aa6
Ryan Gowan <tr_jester@hotmail.com>
2016-02-03 19:11:28 -0500

    Add Hanoi

---
+++ cis237inclass2/Hanoi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass2
+{
+    class Hanoi
+    {
+        public void moveDisk(int n, char source, char auxilary, char destination)
+        {
+            //Baase case to exit recursive loop.
+            if (n == 1)
+            {
+                //Since we only have to move one disk. (n-1), we don't want to make another
+                //recursive call. we jist want to output the move that we are performing.
+                Console.WriteLine("Move disk from tower {0} to tower {1}", source, destination);
+            }
+            else
+            {
+                //Note: the desciption of source, auxilary, and destination are refering to
+                //the first time this moveDisk mothod is called. As recursion works, it may
+                //not make sense to associate the work source with a peg letter.
+                                
+                //Attempt to move (n-1) disks from source location (A) to auxilary location (B), using
+                //the destination(C) peg as temporary storage.
+                moveDisk(n - 1, source, destination, auxilary);
+
+                //Move the Source disk (A) to the Destination (C).
+                moveDisk(1, source, auxilary, destination);
+
+                //Move the (n-1) from Auxilary (B) to Destination (C) using the Beginning
+                //(A) as temporary storage.
+                moveDisk(n - 1, auxilary, source, destination);
+            }
+        }
+    }
+}
--- cis237inclass2/Program.cs
+++ cis237inclass2/Program.cs
@@ -10,7 +10,39 @@ namespace cis237inclass2
     {
         static void Main(string[] args)
         {
-            Factorial factorial = new Factorial();
+            Console.WriteLine("Enter 1 for factorials or 2 for Tower of Hanoi");
+            
+            string userInput = Console.ReadLine();
+            
+            switch (userInput)
+            {
+                case "1":
+                    {
+                        Factorial factorial = new Factorial();
+                        break;
+                    }
+                case "2":
+                    {
+                        Console.WriteLine("Enter number of disks on tower. Try 5 or lower");
+                        userInput = Console.ReadLine();
+                        try
+                        {
+                            //Create a new instance of the Hanoi class
+                            Hanoi hanoi = new Hanoi();
+                            hanoi.moveDisk(Int32.Parse(userInput), 'A', 'B', 'C');
+                        }
+                        catch
+                        {
+                            Console.WriteLine("You must enter an int");
+                        }
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+            
+            
         }
     }
 }
